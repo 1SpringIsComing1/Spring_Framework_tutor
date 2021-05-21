bfa773e1fedf0ab58c4a4227197b13566fb6e655
Maksym_Kruhovykh <Maksym_kruhovykh@epam.com>
2021-05-21 10:48:16 +0300

    Initial commit

---
+++ src/main/java/maksym/kruhovykh/app/SpringFrameworkTutorApplication.java
@@ -0,0 +1,13 @@
+package maksym.kruhovykh.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFrameworkTutorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringFrameworkTutorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/maksym/kruhovykh/app/SpringFrameworkTutorApplicationTests.java
@@ -0,0 +1,13 @@
+package maksym.kruhovykh.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFrameworkTutorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
